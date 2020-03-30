dec96c1b7c1db1f1b2baaebea9b58abc2a74fd79
AKvin <kevin.alvespinto@gmail.com>
2020-03-31 00:12:04 +0200

    #FEAT - Server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
+import bodyParser from 'body-parser';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import hpp from 'hpp';
+import mongoose from 'mongoose';
 import morgan from 'morgan';
+import Player from './models/playerModel';
 import emoji from 'node-emoji';
 import responseTime from 'response-time';
 import favicon from 'serve-favicon';
 import indexRouter from './routes/index';
+import { playerRoute } from './routes/player';
 
-const app = express();
+const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+// load .env
+dotenv.config();
 
 // secure the server by setting various HTTP headers
 app.use(helmet());
@@ -43,6 +53,40 @@ app.use(morgan('dev'));
 // records the response time for HTTP requests
 app.use(responseTime());
 
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// SWAGGER
+const swaggerSpec = require('./src/config/swagger.config').spec();
+
+const swaggerOptions = {
+  customSiteTitle: 'Mern Api mongodb Documentation',
+  customCss: '.topbar { display: none }',
+};
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerOptions));
+app.get('/api-docs.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(swaggerSpec);
+});
+
+console.log(
+  `mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`
+);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  'mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}',
+  {
+    useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true
+  })
+  .then(() => {
+    console.log(emoji.get('white_check_mark'), ' MongoSB connection success !');
+  });
+
 // limit repeated requests to endpoints such as password reset
 app.use(
   new rateLimit({
@@ -54,9 +98,11 @@ app.use(
 
 // routes
 app.use('/', indexRouter);
+const routes = require('./routes/player');
+routes(app);
 
 // setup ip address and port number
-app.set('port', process.env.PORT || 3000);
+app.set('port', port);
 app.set('ipaddr', '0.0.0.0');
 
 // start express server
